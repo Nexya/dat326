cc918719a83433c3915dd13a780110b14feb21d7
Nexya <wallinder.theres@gmail.com>
2022-01-31 11:26:30 +0100

    A1 done

--- assignment1.hs
+++ assignment1.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE FlexibleContexts #-}
-import GHC.Classes (Ord)
-import Data.List
+import Data.List ( intersect, nub, union )
+import Data.Maybe (fromJust)
 -- ASSIGNMENT 1 
 -- dat326
 
@@ -11,22 +10,29 @@ data TERM v = Empty
             | Union        (TERM v) (TERM v)
             | Intersection (TERM v) (TERM v)
             | Var          v
+    deriving (Eq,Show)
 
-data PRED v = Elem     v        (TERM v)
+data PRED v = Elem     (TERM v) (TERM v)
             | Subset   (TERM v) (TERM v)
             | Implies  (PRED v) (PRED v)
             | And      (PRED v) (PRED v)
             | Or       (PRED v) (PRED v)
             | Not      (PRED v)
+    deriving (Eq,Show)
 
 -- Part 2
 
 newtype Set = S [Set]
+    deriving (Eq,Show)
+
 
 type Env var dom = [(var,dom)]
 
+{-- 
 rmDupl :: (Eq a, Ord a) => [a] -> [a]
 rmDupl = map head . group . sort
+--}
+
 
 -- helpers
 lift :: Set -> [Set]
@@ -36,23 +42,39 @@ lift (S x) = x
 eval :: Eq v => Env v Set -> TERM v -> Set
 eval env Empty                = S []
 eval env (Singleton t)        = S [eval env t]
-eval env (Union t1 t2)        = S [] 
--- $ rmDupl $ lift (eval env t1) ++ lift (eval env t2)
-eval env (Intersection t1 t2) = S []
+eval env (Union t1 t2)        = S $ nub $ lift (eval env t1) `union` lift (eval env t2)
+eval env (Intersection t1 t2) = S $ nub $ lift (eval env t1) `intersect` lift (eval env t2)
 -- $ filter (\x -> x `elem` lift (eval env t1)) (lift(eval env t2))
-eval env (Var v)              = S [] -- TODO
+eval env (Var v)              = fromJust $ lookup v env
 
 -- check
 (==>) :: Bool -> Bool -> Bool
 False ==> _ = True
 True  ==> p = p
 
+contains :: Set -> Set -> Bool
+contains e (S (x : xs))
+  | e == x    = True
+  | otherwise = contains e (S xs)
+contains e (S []) = False
+
 check :: Eq v => Env v Set -> PRED v -> Bool
-check env (Elem v t)      = True  -- TODO
-check env (Subset t1 t2)  = True  -- TODO
+check env (Elem v t)      = contains (eval env v) (eval env t)
+check env (Subset t1 t2)  = all (\x -> contains x $ eval env t2) $ lift $ eval env t1
 check env (Implies p1 p2) = check env p1 ==> check env p2
 check env (And p1 p2)     = check env p1  && check env p2
 check env (Or p1 p2)      = check env p1  || check env p2
-check env (Not p)         = not (check env p)
+check env (Not p)         = not $ check env p
 
 -- Part 3
+
+vonNeumann :: Integer -> TERM Integer
+vonNeumann 0 = Empty
+vonNeumann n = Union (vonNeumann (n-1)) (Singleton (vonNeumann (n-1)))
+
+
+claim1 :: Integer -> Integer -> Bool
+claim1 n1 n2 = (n1 <= n2) ==> check [] (vonNeumann n1 `Subset` vonNeumann n2)
+
+claim2 :: Integer -> Bool
+claim2 n = eval [] (vonNeumann n) == S (map (eval [] . vonNeumann) [0..(n-1)])
