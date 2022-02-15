bf599fd36617bb1f158c6924f8e28a1bdbc02606
Nexya <wallinder.theres@gmail.com>
2022-02-15 13:58:55 +0100

    A2 started and support files added

---
+++ assignment2.hs
@@ -0,0 +1,38 @@
+-- Recommended skeleton code
+
+{-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+module A2 where
+import Prelude hiding ((+),(-),(*),(/),negate,recip,(^),
+                        pi, sin,cos,exp,fromInteger,fromRational)
+import DSLsofMath.FunExp ()
+import DSLsofMath.Algebra
+    ( Transcendental(..),
+      MulGroup(recip),
+      Multiplicative(..),
+      AddGroup(..),
+      Additive(..) ) 
+
+
+type Tri a    = (a,a,a)
+type TriFun a = Tri (a -> a) -- = (a -> a, a -> a, a -> a)
+type FunTri a = a -> Tri a   -- = a -> (a,a,a)
+
+
+instance Additive a => Additive (Tri a) where
+    (+) = addTri; zero = zeroTri
+instance (Additive a, Multiplicative a) => Multiplicative (Tri a) where
+    (*) = mulTri; one = oneTri
+instance AddGroup a => AddGroup (Tri a) where
+    negate = negateTri
+instance (AddGroup a, MulGroup a) => MulGroup (Tri a) where
+    recip = recipTri
+(addTri, zeroTri, mulTri, oneTri, negateTri, recipTri) = undefined
+
+instance Transcendental a => Transcendental (Tri a) where
+    pi = piTri; sin = sinTri; cos = cosTri; exp = expTri;
+(piTri, sinTri, cosTri, expTri) = undefined;
+
+
+-- Part 1
+
