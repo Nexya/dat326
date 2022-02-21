ba4a18659696c91159f8da382fccee21ccd1784e
Nexya <wallinder.theres@gmail.com>
2022-02-21 10:37:30 +0100

    1a and 1b started

--- assignment1.hs
+++ assignment1.hs
@@ -1,82 +1,126 @@
-import Data.List ( intersect, nub, union )
-import Data.Maybe (fromJust)
--- ASSIGNMENT 1 
--- dat326
-
--- Part 1
-
-data TERM v = Empty
-            | Singleton    (TERM v)
-            | Union        (TERM v) (TERM v)
-            | Intersection (TERM v) (TERM v)
-            | Var          v
-    deriving (Eq,Show)
-
-data PRED v = Elem     (TERM v) (TERM v)
-            | Subset   (TERM v) (TERM v)
-            | Implies  (PRED v) (PRED v)
-            | And      (PRED v) (PRED v)
-            | Or       (PRED v) (PRED v)
-            | Not      (PRED v)
-    deriving (Eq,Show)
-
--- Part 2
-
-newtype Set = S [Set]
-    deriving (Eq,Show)
-
-
-type Env var dom = [(var,dom)]
-
-{--  used nub instead
-rmDupl :: (Eq a, Ord a) => [a] -> [a]
-rmDupl = map head . group . sort
+{--
+  Group: a1.07
+  Group member: Theres Wallinder, Pavlos Stampoulis, Jesper FÃ¼hr
 --}
 
+import Data.List
+import Data.Maybe (fromJust)
 
--- helpers
+-------- PART 1
+data TERM a
+  = Empty
+  | Singleton (TERM a)
+  | Union (TERM a) (TERM a)
+  | Intersection (TERM a) (TERM a)
+  | TV a
+  deriving (Show)
+
+data PRED a
+  = Elem (TERM a) (TERM a)
+  | Subset (TERM a) (TERM a)
+  | And (PRED a) (PRED a)
+  | Or (PRED a) (PRED a)
+  | Implies (PRED a) (PRED a)
+  | Not (PRED a)
+  deriving (Show)
+
+-------- PART 2
+
+newtype Set = S [Set]
+  deriving (Show)
+instance Eq Set where
+  (==) (S []) (S []) = True
+  (==)  _     (S []) = False
+  (==) (S [])  _     = False
+  (==) (S a)  (S b)  =
+    all (`elem` b) a
+      && all (`elem` a) b
+
+type Env var dom = [(var, dom)]
+
+eval :: Eq v => Env v Set -> TERM v -> Set
+eval env Empty = S []
+eval env (Singleton a) = S [eval env a]
+eval env (TV a) = fromJust $ lookup a env
+eval env (Union a b) = S $ nub $ lift (eval env a) `union` lift (eval env b) -- was incorrect due to eq
+eval env (Intersection a b) = S $ nub $ lift (eval env a) `intersect` lift (eval env b) -- was incorrect due to eq
+
+check :: Eq v => Env v Set -> PRED v -> Bool
+check env (Elem a b) = contains (eval env a) (eval env b)
+check env (Subset a b) = all (\x -> contains x $ eval env b) $ lift $ eval env a
+check env (And a b) = check env a && check env b
+check env (Or a b) = check env a || check env b
+check env (Implies a b) = check env a `implies` check env b
+check env (Not a) = not $ check env a
+
+-------- PART 3
+
+vonNeumann :: Integer -> TERM a
+vonNeumann 0 = Empty
+vonNeumann n = Union a (Singleton a)
+  where
+    a = vonNeumann (n - 1)
+
+claim1 :: Integer -> Integer -> Bool
+claim1 n1 n2 = (n1 <= n2) `implies` check [("h", S [])] (Subset a b)
+  where
+    a = vonNeumann n1
+    b = vonNeumann n2
+
+claim2 :: Integer -> Bool
+claim2 n = eval a (vonNeumann n) == S (map (eval a . vonNeumann) [0 .. (n -1)])
+  where
+    a = [("h", S [])]
+
+---- Help thingies
 lift :: Set -> [Set]
 lift (S x) = x
 
--- eval 
-eval :: Eq v => Env v Set -> TERM v -> Set
-eval env Empty                = S []
-eval env (Singleton t)        = S [eval env t]
-eval env (Union t1 t2)        = S $ nub $ lift (eval env t1) `union` lift (eval env t2) -- can use ++ instead
-eval env (Intersection t1 t2) = S $ nub $ lift (eval env t1) `intersect` lift (eval env t2)
--- $ filter (\x -> x `elem` lift (eval env t1)) (lift(eval env t2))   intersection variant
-eval env (Var v)              = fromJust $ lookup v env
-
--- check
-(==>) :: Bool -> Bool -> Bool
-False ==> _ = True
-True  ==> p = p
 
 contains :: Set -> Set -> Bool
-contains e (S (x : xs))
-  | e == x    = True
-  | otherwise = contains e (S xs)
-contains e (S []) = False
+contains e (S x) = e `elem` x
 
-check :: Eq v => Env v Set -> PRED v -> Bool
-check env (Elem v t)      = contains (eval env v) (eval env t)
-check env (Subset t1 t2)  = all (\x -> contains x $ eval env t2) $ lift $ eval env t1
-check env (Implies p1 p2) = check env p1 ==> check env p2
-check env (And p1 p2)     = check env p1  && check env p2
-check env (Or p1 p2)      = check env p1  || check env p2
-check env (Not p)         = not $ check env p
+implies :: Bool -> Bool -> Bool
+True `implies` x = x
+False `implies` _ = True
 
--- Part 3
+cardinality :: Set -> Integer
+cardinality (S x) = toInteger $ length x
 
-vonNeumann :: Integer -> TERM Integer
-vonNeumann 0 = Empty
-vonNeumann n = Union (vonNeumann (n-1)) (Singleton (vonNeumann (n-1)))
+{--
+  Used for tests
+--}
 
--- om tal 1 är mindre än tal 2 så kommer tal 1 vara ett subset av tal 2
-claim1 :: Integer -> Integer -> Bool
-claim1 n1 n2 = (n1 <= n2) ==> check [] (vonNeumann n1 `Subset` vonNeumann n2)
+big :: (Eq t, Num t) => t -> TERM a
+big 0 = Empty
+big n = Singleton (big (n -1))
 
--- 
-claim2 :: Integer -> Bool
-claim2 n = eval [] (vonNeumann n) == S (map (eval [] . vonNeumann) [0..(n-1)])
+-- Should return a set of sets of depth 0 to n-1
+uni :: (Eq t, Num t) => t -> TERM a
+uni 0 = Empty
+uni n = Union (big n) (uni (n -1))
 
+uni' :: (Eq t, Num t) => t -> TERM a
+uni' 0 = Empty
+uni' n = Union (uni (n -1)) (big n)
+
+int :: (Eq t, Num t) => t -> TERM a
+int 0 = Empty
+int n = Intersection (big n) (int (n -1))
+
+-- should evaluate to Singleton Empty represented in Set
+test :: TERM a
+test = Intersection (Intersection (Singleton Empty) (Union (Singleton Empty) Empty)) (Singleton Empty)
+
+a = S [S [], S []]
+
+b = S [S []]
+
+c = S [a, a, b]
+
+d = S [a, b, a]
+
+e = S [a, b, b]
+
+-- Should return true, at all times
+testeroo = a == b
--- assignment2.hs
+++ assignment2.hs
@@ -19,7 +19,7 @@ import DSLsofMath.Algebra
       MulGroup(recip),
       Multiplicative(..),
       AddGroup(..),
-      Additive(..) ) 
+      Additive(..) )
 
 
 type Tri a    = (a,a,a)
@@ -35,7 +35,7 @@ instance AddGroup a => AddGroup (Tri a) where
     negate = negateTri
 instance (AddGroup a, MulGroup a) => MulGroup (Tri a) where
     recip = recipTri
-(addTri, zeroTri, mulTri, oneTri, negateTri, recipTri) = undefined
+(oneTri, negateTri, recipTri) = undefined
 
 instance Transcendental a => Transcendental (Tri a) where
     pi = piTri; sin = sinTri; cos = cosTri; exp = expTri;
@@ -47,3 +47,31 @@ instance Transcendental a => Transcendental (Tri a) where
 
 eval'' :: Transcendental a => FunExp -> (a -> a)
 eval'' = eval' . derive
+
+
+-- part (a)
+
+-- example (exponential function): 
+--      exp (a + b) == exp a * exp b
+
+-- following test should hold (example from lecture 4.1.1) if homomorphism
+h2 :: (Transcendental b, Eq b) => (a -> b, a -> a -> a, b -> b -> b) -> a -> a -> Bool
+h2 (h, op1, op2) x y = h (op1 x y) == op2 (h x) (h y)
+
+-- to prove that eval'' (second derivative) isn't a homomorphism then we need to find a case where 
+--      eval'' (f op1 g) \= (eval'' f) op2 (eval'' g)
+-- where op1 (syntactic domain) directly correstponds to op2 (semantic domain)
+
+
+
+
+-- part (b)
+-- tri instances
+
+addTri :: (Additive a) => Tri a -> Tri a -> Tri a
+addTri (a,b,c) (a',b',c') = (a + a', b + b', c + c') 
+
+zeroTri :: (Additive  a) => Tri a
+zeroTri = (zero, zero, zero)
+
+mulTri = undefined 
